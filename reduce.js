82693450681b2772e22268ad8b4170ae8ebc5d1b
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-17 22:22:55 +0000

    higher other function

---
+++ reduce.js
@@ -0,0 +1,29 @@
+var kodeUnixRandom = [
+  12,
+  78,
+  42,
+  40,
+  36,
+  2,
+  8,
+  96,
+  66,
+  20,
+  91,
+  85,
+  41,
+  69,
+  38,
+  77,
+  97,
+  60,
+  15,
+  57,
+];
+
+const randomUnix = kodeUnixRandom.reduce(
+  (x, y) => (x + y) / kodeUnixRandom.length
+);
+// urutan aritmatika perhitungan = x , / , + , -
+
+console.log(randomUnix);
